3e325e4701ccd73ede8b02fc85606b3000ed014c
Tyla Devon <tyladevon@gmail.com>
2020-01-14 14:13:43 -0700

    Continue working on gif poro to return a hash of required information.

--- app/controllers/api/v1/gif_controller.rb
+++ app/controllers/api/v1/gif_controller.rb
@@ -2,17 +2,12 @@ class Api::V1::GifController < ApplicationController
   def show
     get_local = GoogleGeocodeService.new.get_location(params[:location])
     info_for_darksky = Location.new(get_local)
-    @forecast_data = DarkskyService.new.get_forecast(ENV['darksky_api_key'], info_for_darksky.lat, info_for_darksky.long)
+    forecast_data = DarkskyService.new.get_forecast(ENV['darksky_api_key'], info_for_darksky.lat, info_for_darksky.long)
     gif_forecast = GifService.new.get_gif(forecast_data[:currently][:summary])
-    gif = Gif.new(gif_forecast[:data][0][:url])
-
+    gif = Gif.new(gif_forecast[:data][0][:url],forecast_data).get_response
 
     render json: GifSerializer.new(gif, forecast_data[])
   end
 
-  private
 
-  def get_gif_time
-    
-  end
 end
--- app/models/gif.rb
+++ app/models/gif.rb
@@ -1,6 +1,19 @@
 class Gif
-  def initialize(url)
+  def initialize(url, forecast_data)
     @id = nil
     @url = url
+    @forecast_data = forecast_data
   end
+
+  def get_response
+    response = @forecast_data[:hourly][:data].each do |forecast|
+      # binding.pry
+      forecast[:time]
+      forecast[:summary]
+      @url
+    end
+    binding.pry
+    response
+  end
+
 end
